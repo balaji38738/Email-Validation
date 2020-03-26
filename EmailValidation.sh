b0558ac214b1277ff37a78933192355ac0ff3e4f
balaji38738 <bijjapwar123@gmail.com>
2020-03-27 04:27:00 +0530

    Regex to validate second optional part

--- EmailValidation.sh
+++ EmailValidation.sh
@@ -5,7 +5,7 @@
 #Date:- 27 March 2020
 
 printf "Email validation programme\n\n"
-emailPattern="^[0-9a-zA-Z]+([_+-.][0-9a-zA-Z]+)*@[0-9a-zA-Z]+[.][a-zA-Z]+"
+emailPattern="^[0-9a-zA-Z]+([_+-.][0-9a-zA-Z]+)*@[0-9a-zA-Z]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})?$"
 read -p "Enter email id: " email
 
 if [[ $email =~ $emailPattern ]]
